fe56c1594e859aef5bd783eb12b98246d312d0c7
imanuelvictory98 <59839361+imanuelvictory98@users.noreply.github.com>
2020-07-02 01:47:00 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Share+Tech+Mono'>
 <link rel="stylesheet" href="./css/style.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="./css/img.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="manifest" href="/manifest.json">
 <meta name="Description" content="Meeting Schedjule" />
 <!-- Mendeklarasikan warna yang muncul pada address bar Chrome versi seluler -->
@@ -31,30 +32,28 @@
   
   <p><img src="img/logo/merah.png" style="max-width:250px;"></p>
   
- <div class="content" style="color: #8B254F">
-    <div class="indx-1 indx-s-2" > 
+<div class="indx-1 indx-s-2" style="color: #8B254F; padding: 10px 20px;" > 
       <h1 style="text-align: left;">Welcome to Puri Indah </h1>
 	  	<h5 style="text-align:justify;"><p>A great location in West Jakarta's bustling Puri Indah with fast and easy access to Jakarta - Tangerang Toll Road, Kebon Jeruk and the many business on and around Daan Mogot, make This "fave" one of Jakartas's favorite select service hotels. The 108 stylist rooms are modern, functional and super clean offering cutting-edge LCD TVs, fast and reliable complimentary WiFi, security key card system, air conditioning, in room saves and top of the line and beds with high knot count cotton linens.</p>
 		<br>
 		Jl. Kembang Abadi Utama Jl. Berlian Raya No.1, Kembangan Sel., Kec. Kembangan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11610
 		<br><br>
-		T	: +62 21 580 7711<br>
-		E	: puriindahinfo@favehotels.com<br>
-		W	: puriindahjakarta.favehotels.com<br>
+		<a href="tel:+62215807711"  style="text-decoration:none"><img src="img/sosmed/tlp.jpg" style="width:32px;"> +62 21 580 7711</a><br>
+		<a href="mailto:puriindahinfo@favehotels.com"  style="text-decoration:none"><img src="img/sosmed/em.png" style="width:27px;"> puriindahinfo@favehotels.com</a><br>
+		<a href="https://puriindahjakarta.favehotels.com"  style="text-decoration:none"><img src="img/sosmed/em.png" style="width:27px;"> puriindahjakarta.favehotels.com</a><br>
 		</h5> 
-	</div>
+</div>
     
-    <div class="indx-1 indx-s-2" >
-    <div class="content">
-	<h2 style="text-align: Center;">Today Promo </h2>
-      <div class="w3-content w3-section" style="max-width:400px; text-align:center;">
-        <img class="mySlides w3-animate-fading" src="img/promo/1.jpg" style="width:100%">
-        <img class="mySlides w3-animate-fading" src="img/promo/2.jpg" style="width:100%">
-        <img class="mySlides w3-animate-fading" src="img/promo/3.jpg" style="width:100%">
-        <img class="mySlides w3-animate-fading" src="img/promo/4.jpg" style="width:100%">
-      </div>
-  
-    <script>
+<div class="indx-1 indx-s-2" style="color: #8B254F;" >
+<h1>Today Promo ..!!</h1>
+<div class="w3-content w3-section" style="max-width:440px; text-align:center;">
+    <img class="mySlides w3-animate-fading" src="img/promo/1.jpg" style="width:100%">
+    <img class="mySlides w3-animate-fading" src="img/promo/2.jpg" style="width:100%">
+    <img class="mySlides w3-animate-fading" src="img/promo/3.jpg" style="width:100%">
+    <img class="mySlides w3-animate-fading" src="img/promo/4.jpg" style="width:100%">
+</div>
+
+<script>
 var myIndex = 0;
 carousel();
 
@@ -71,9 +70,8 @@ function carousel() {
 }
 </script>
       
-   </div>
-  </div>
 </div>
+
 
 
 <div class="indx-3 indx-s-2" style="text-align:justify; background: #8B254F; padding: 10px 20px;">
@@ -125,19 +123,49 @@ Roof Top at "R Floor"
 
 <div class="indx-4 indx-s-2" style="background: #8B254F;">   
    <h2 style="text-align: center;">
-      How Can We Help You ...?
-	  <br>
-	  <img src="img/sosmed/ig.png" alt="Avatar" class="avatar">
-	  <img src="img/sosmed/ig.png" alt="Avatar" class="avatar">
-	</h2> 
-</div>
-	  
-
+      Enjoy Our Facilites
+   </h2> 
  
+   <div class="v-container">
+	 <iframe src="//www.youtube.com/embed/S9-9leT1LLE?autoplay=1" frameborder="0" allowfullscreen class="video"></iframe>
+   </div>
+
+</div>
+	   
+<div class="indx-1 indx-s-2" style="text-align:center; color: #8B254F; padding: 10px 20px;; max-height:250px; ">
+<h1> Wifi information </h1>
+<img src="img/wifi logo.jpg" style="max-width : 250px;">
+<img src="img/qr-code.png" style="max-width : 275px;"><br>
+scan it to connect WIFI
 </div>  
-  
- <div class="footer" style="color: #8B254F">
-		<br>
+<div class="indx-1 indx-s-2" style="text-align:justify; color: #8B254F; padding: 10px 20px;; max-height:250px;">
+<P>
+	<image src="img/sosmed/ig.png" onclick="document.getElementById('id01').style.display='block'" style="max-width:80px;"><span>
+	<image src="img/sosmed/ig.png" onclick="document.getElementById('id01').style.display='block'" style="max-width:80px;"><span>
+</P>
+<div id="id01" class="modal">
+	<p>
+	<img src="img/menu/3.jpeg" style="max-width: 380px;">
+	<img src="img/menu/1.jpeg" style="max-width: 380px;">
+	<img src="img/menu/2.jpeg" style="max-width: 360px;">
+	</p>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+</div>
+
+</div> 
+<div class="footer" style="color: #8B254F">
 		<br>
 		<p>
 			puriindahjakarta.favehotels.com 
@@ -154,6 +182,21 @@ Roof Top at "R Floor"
   }
 </script>
 
+<script> 
+window.intergramId = "856756027"
+window.intergramCustomizations = {
+        titleClosed: 'Live Chat',
+        titleOpen: 'favehotel Puri Indah',
+        introMessage: 'Welcame to favehotel Puri Indah Jakarta',
+        autoResponse: 'Thankyou to contact us, we will reply your massege soon.',
+        autoNoResponse: 'Thankyou to contact us, we will reply your massege soon. ' +
+                        'Thankyou to contact us, we will reply your massege soon.',
+        mainColor: "#8B254F", // Can be any css supported color 'red', 'rgb(255,87,34)', etc
+        alwaysUseFloatingButton: false // Use the mobile floating button also on large screens
+    };
+</script>
+
+<script id="intergram" type="text/javascript" src="https://www.intergram.xyz/js/widget.js"></script>
 </body>
 </html>
 
